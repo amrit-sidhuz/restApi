63d5350caff16a1559212d66199f3c70505c0542
Amrit Sidhu <amritsidhu98@gmail.com>
2022-08-05 12:07:36 +0530

    DATABASE_URL added

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(express.static(join(process.cwd(), "public")));
 app.use(express.urlencoded({ extended: true }));
 // app.use(bodyParser.urlencoded({ extended: true }));
 
-const DATABASE_URL = "mongodb+srv://AmritSidhu:amrit.sidhu365@cluster0.nhoxgg4.mongodb.net/?retryWrites=true&w=majority";
+const DATABASE_URL = process.env.DATABASE_URL || "mongodb+srv://AmritSidhu:amrit.sidhu365@cluster0.nhoxgg4.mongodb.net/?retryWrites=true&w=majority";
 connectDB(DATABASE_URL);
 
 app.set("views", "./views");
